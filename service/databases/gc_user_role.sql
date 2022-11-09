51633ac6173a6f69b7e9a63fbd91552fdd25fb81
梦工厂 <sonhineboy@163.com>
2022-11-09 08:44:47 +0800

    初始化数据库

---
+++ service/databases/gc_user_role.sql
@@ -0,0 +1,7 @@
+INSERT INTO `gc_user_role`(`admin_user_id`, `role_id`) VALUES (57, 14);
+INSERT INTO `gc_user_role`(`admin_user_id`, `role_id`) VALUES (74, 14);
+INSERT INTO `gc_user_role`(`admin_user_id`, `role_id`) VALUES (75, 14);
+INSERT INTO `gc_user_role`(`admin_user_id`, `role_id`) VALUES (77, 14);
+INSERT INTO `gc_user_role`(`admin_user_id`, `role_id`) VALUES (54, 15);
+INSERT INTO `gc_user_role`(`admin_user_id`, `role_id`) VALUES (75, 15);
+INSERT INTO `gc_user_role`(`admin_user_id`, `role_id`) VALUES (76, 15);
