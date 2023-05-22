5da1c6275467768cc9a1c757b34f61554a62dab4
keivn <172205267@qq.com>
2023-05-22 22:36:10 +0800

    log

--- web/scui/src/api/model/system.js
+++ web/scui/src/api/model/system.js
@@ -6,21 +6,21 @@ export default {
 		myMenus: {
 			url: `${config.API_URL}/system/menu/my/1.6.1`,
 			name: "获取我的菜单",
-			get: async function(){
+			get: async function () {
 				return await http.get(this.url);
 			}
 		},
 		list: {
 			url: `${config.API_URL}/system/menu/list`,
 			name: "获取菜单",
-			get: async function(){
+			get: async function () {
 				return await http.get(this.url);
 			}
 		},
-		dels:{
+		dels: {
 			url: `${config.API_URL}/system/menu/dels`,
 			name: "删除菜单",
-			post: async function(data){
+			post: async function (data) {
 				return await http.post(this.url, data, {
 					headers: {
 						//'response-status': 401
@@ -28,10 +28,10 @@ export default {
 				});
 			}
 		},
-		add:{
+		add: {
 			url: `${config.API_URL}/system/menu/add`,
 			name: "添加菜单",
-			post: async function(data){
+			post: async function (data) {
 				return await http.post(this.url, data, {
 					headers: {
 						//'response-status': 401
@@ -39,10 +39,10 @@ export default {
 				});
 			}
 		},
-		updata:{
+		updata: {
 			url: `${config.API_URL}/system/menu/up`,
 			name: "添加菜单",
-			post: async function(data){
+			post: async function (data) {
 				return await http.post(this.url, data, {
 					headers: {
 						//'response-status': 401
@@ -56,21 +56,21 @@ export default {
 		tree: {
 			url: `${config.API_URL}/system/dic/tree`,
 			name: "获取字典树",
-			get: async function(){
+			get: async function () {
 				return await http.get(this.url);
 			}
 		},
 		list: {
 			url: `${config.API_URL}/system/dic/list`,
 			name: "字典明细",
-			get: async function(params){
+			get: async function (params) {
 				return await http.get(this.url, params);
 			}
 		},
 		get: {
 			url: `${config.API_URL}/system/dic/get`,
 			name: "获取字典数据",
-			get: async function(params){
+			get: async function (params) {
 				return await http.get(this.url, params);
 			}
 		}
@@ -79,35 +79,35 @@ export default {
 		list: {
 			url: `${config.API_URL}/system/role/list`,
 			name: "获取角色列表",
-			get: async function(params){
+			get: async function (params) {
 				return await http.get(this.url, params);
 			}
 		},
-		upMenus : {
+		upMenus: {
 			url: `${config.API_URL}/system/role/upMenu`,
 			name: "添加角色",
-			post: async function(params){
+			post: async function (params) {
 				return await http.post(this.url, params);
 			}
 		},
 		add: {
 			url: `${config.API_URL}/system/role/add`,
 			name: "添加角色",
-			post: async function(params){
+			post: async function (params) {
 				return await http.post(this.url, params);
 			}
 		},
 		up: {
 			url: `${config.API_URL}/system/role/up`,
 			name: "更新角色",
-			post: async function(params){
+			post: async function (params) {
 				return await http.post(this.url, params);
 			}
 		},
 		del: {
 			url: `${config.API_URL}/system/role/del`,
 			name: "del角色",
-			post: async function(params){
+			post: async function (params) {
 				return await http.post(this.url, params);
 			}
 		},
@@ -116,7 +116,7 @@ export default {
 		list: {
 			url: `${config.API_URL}/system/dept/list`,
 			name: "获取部门列表",
-			get: async function(params){
+			get: async function (params) {
 				return await http.get(this.url, params);
 			}
 		}
@@ -125,7 +125,7 @@ export default {
 		list: {
 			url: `${config.API_URL}/system/user/list`,
 			name: "获取用户列表",
-			get: async function(params){
+			get: async function (params) {
 				return await http.get(this.url, params);
 			}
 		}
@@ -134,7 +134,7 @@ export default {
 		list: {
 			url: `${config.API_URL}/system/app/list`,
 			name: "应用列表",
-			get: async function(){
+			get: async function () {
 				return await http.get(this.url);
 			}
 		}
@@ -143,7 +143,7 @@ export default {
 		list: {
 			url: `${config.API_URL}/system/log/list`,
 			name: "日志列表",
-			get: async function(params){
+			get: async function (params) {
 				return await http.get(this.url, params);
 			}
 		}
@@ -152,14 +152,14 @@ export default {
 		list: {
 			url: `${config.API_URL}/system/table/list`,
 			name: "表格列管理列表",
-			get: async function(params){
+			get: async function (params) {
 				return await http.get(this.url, params);
 			}
 		},
 		info: {
 			url: `${config.API_URL}/system/table/info`,
 			name: "表格列管理详情",
-			get: async function(params){
+			get: async function (params) {
 				return await http.get(this.url, params);
 			}
 		}
@@ -168,9 +168,18 @@ export default {
 		list: {
 			url: `${config.API_URL}/system/tasks/list`,
 			name: "系统任务管理",
-			get: async function(params){
+			get: async function (params) {
 				return await http.get(this.url, params);
 			}
 		}
+	},
+	systemLog: {
+		list: {
+			url: `${config.API_URL}/system/operationLog/list`,
+			name: "获取角色列表",
+			get: async function (params) {
+				return await http.get(this.url, params);
+			}
+		},
 	}
 }
---
+++ web/scui/src/views/setting/systemLog/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <el-container>
+        <el-container>
+            <el-header>
+                <div class="left-panel">
+                    <el-button v-if="this.$AUTH('user.add')" type="primary" icon="el-icon-plus" @click="add"></el-button>
+                    <el-button v-if="this.$AUTH('user.del')" type="danger" plain icon="el-icon-delete"
+                        :disabled="selection.length == 0" @click="batch_del"></el-button>
+                    <!-- <el-button type="primary" plain :disabled="selection.length==0">分配角色</el-button>
+                          <el-button type="primary" plain :disabled="selection.length==0">密码重置</el-button> -->
+                </div>
+                <div class="right-panel">
+                    <div class="right-panel-search">
+                        <el-input v-model="search.name" placeholder="登录账号 / 姓名" clearable></el-input>
+                        <el-button type="primary" icon="el-icon-search" @click="upsearch"></el-button>
+                    </div>
+                </div>
+            </el-header>
+            <el-main class="nopadding">
+                <scTable ref="table" :apiObj="apiObj" @selection-change="selectionChange" stripe remoteSort remoteFilter>
+                    <!-- <el-table-column label="ID" prop="id" width="80"></el-table-column> -->
+                    <el-table-column label="#" type="index" width="50"></el-table-column>
+                    <el-table-column label="用户名" prop="user_name" width="80"></el-table-column>
+                    <el-table-column label="方法" prop="method" width="80"></el-table-column>
+                    <el-table-column label="访问路径" prop="path_name" width="140"></el-table-column>
+                    <el-table-column label="IP" prop="ip" width="120"></el-table-column>
+                    <el-table-column label="数据" prop="do_data"></el-table-column>
+                    <el-table-column label="创建时间" prop="created_at" width="150"></el-table-column>
+                    <!-- <el-table-column label="操作" fixed="right" align="right" width="160"> -->
+                        <!-- <template #default="scope">
+                            <el-button-group>
+                                <el-button text type="primary" size="small">查看</el-button>
+                            </el-button-group>
+                        </template> -->
+                    <!-- </el-table-column> -->
+                </scTable>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+  
+<script>
+
+export default {
+    name: "systemLog",
+
+    data() {
+        return {
+            apiObj: this.$API.system.systemLog.list,
+            selection: [],
+            search: {
+                name: null,
+            },
+
+        };
+    },
+    watch: {
+
+    },
+    mounted() {
+        // this.getGroup()
+
+        console.log(this.$AUTH("user.add"))
+    },
+    methods: {
+
+    },
+};
+</script>
+  
+<style></style>
+  
